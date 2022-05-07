7856c06e35d17ebac77775b70c4f0d60223d47d0
Vinicius de Oliveira Silva <vinioliveira.s@hotmail.com>
2022-05-07 14:53:33 -0300

    Ajustes

--- runScript.sh
+++ runScript.sh
@@ -3,7 +3,7 @@ for ((j = 200; j <= 2000; j+=200))
     do
     for ((i=0; i < 10; i++))
         do
-        ./multmat.x $j $j $j $j o >> secondRun/o$j.txt
-        ./multmat.x $j $j $j $j t >> secondRun/t$j.txt
+        ./multmat.x $j $j $j $j o >> thirdRun/o$j.txt
+        ./multmat.x $j $j $j $j t >> thirdRun/t$j.txt
         done
     done
