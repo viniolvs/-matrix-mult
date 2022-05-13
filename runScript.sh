b3f8a5bb25425fc2756c965f4c6f5f73dbb5adaf
Vinicius de Oliveira Silva <vinioliveira.s@hotmail.com>
2022-05-13 15:03:52 -0300

    notebook run

--- runScript.sh
+++ runScript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-gcc -Wall -O3 multmat.c -o multmat.x
-#gcc -Wall multmat.c -o multmat.x
-DIRETORIO="NotebookO3"
-for ((j = 200; j <= 2000; j+=200))
+#gcc -Wall -O3 multmat.c -o multmat.x
+gcc -Wall multmat.c -o multmat.x
+DIRETORIO="Notebook"
+for ((j = 1800; j <= 2000; j+=200))
     do
     for ((i=0; i < 10; i++))
         do
