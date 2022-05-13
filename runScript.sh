1f42a31ac44d06de8c554b231a29db80b7168b80
Vinicius de Oliveira Silva <vinioliveira.s@hotmail.com>
2022-05-13 17:24:25 -0300

    testes done

--- runScript.sh
+++ runScript.sh
@@ -2,7 +2,7 @@
 #gcc -Wall -O3 multmat.c -o multmat.x
 gcc -Wall multmat.c -o multmat.x
 DIRETORIO="Notebook"
-for ((j = 1800; j <= 2000; j+=200))
+for ((j = 200; j <= 2000; j+=200))
     do
     for ((i=0; i < 10; i++))
         do
